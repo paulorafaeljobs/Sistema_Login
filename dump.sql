f59825ebfc02b90b1075819da6798d340a9b002c
Paulo Rafael <77584278+paulorafaeljobs@users.noreply.github.com>
2021-02-15 13:07:03 -0300

    Add files via upload

---
+++ dump.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE login;
+USE login;
+
+CREATE TABLE `login`.`usuario` (
+  `usuario_id` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(200) NOT NULL,
+  `senha` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`usuario_id`));
+
+
+
+
+INSERT INTO `usuario` (`usuario`,`senha`) VALUES ('paulo','202cb962ac59075b964b07152d234b70');
+
+
+Usuários/senha:
+
+paulo / 123
